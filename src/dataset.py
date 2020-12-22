f72b6f0f352a5553b219993d12d4ee9427c810bc
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-12-22 01:50:44 -0500

    Encoded labels

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,5 @@
 # %%
+from sklearn import preprocessing
 from genericpath import exists
 import torch
 import os
@@ -34,7 +35,9 @@ def read_data():
     return data
 
 task_data = read_data()
-
+# %%
+task_data.keys()
+# %%
 # %% Experimental Setup data
 def parse_line(line, task):
     spec, label = line.split()
@@ -84,15 +87,28 @@ def read_experimental_setup():
 # tasks, tasks_setup = experimental_setup()
 exp_setup = read_experimental_setup()
 # %%
+gg = [f"G{i}" for i in range(1,16)]
+gg
+def oneint(value, all_values):
+    i = all_values.index(value)
+    le = preprocessing.LabelEncoder()
+    targets = le.fit_transform(all_values)
+    return targets[i]
 
-# %%
+oneint("G1", gg)
 
-# %%
 class JigsawsDataset(Dataset):
     def __init__(self, task="Knot_Tying", user_out="1_Out", itr="itr_1", train=True):        
         self.task = task
         self.user_out = user_out
         self.itr = itr
+        
+        self.gestures = [f"G{i}" for i in range(1, 15+1)]
+
+        le = preprocessing.LabelEncoder()
+        self.targets = le.fit_transform(self.gestures)
+    
+
         if train:
             self.mode = 'Train.txt'
         else:
@@ -108,7 +124,12 @@ class JigsawsDataset(Dataset):
 
         from_line, to_line = e['from_line'], e['to_line']
         trial_fname = f"{self.task}_{trial}.txt"
-        return {'segment' : self.task_data[self.task][trial_fname][from_line:to_line]}
+
+        segment = self.task_data[self.task][trial_fname][from_line:to_line]
+        label = e['label']
+        i = self.gestures.index(label)
+        label_int = self.targets[i]
+        return {'segment' : segment, 'label': label_int}
 
     def __len__(self):
         return len(self.experimental_setup[self.task][self.user_out][self.itr][self.mode])
@@ -120,9 +141,38 @@ class JigsawsDataset(Dataset):
 # train_txt = os.path.join(path, "Train.txt")
 ds = JigsawsDataset()
 # %%
-ds[0:1]
+ds[1]
 # %%
-dl = DataLoader(ds, batch_size=2)
+dl = DataLoader(ds)
+d = next(iter(dl))
+d['segment'].shape
+
 # %%
-next(iter(dl))
+# dl = DataLoader(ds, batch_size=2)
+from torch.nn.utils.rnn import pad_sequence
+
+def pad_collate(batch):
+    yy = [b['label'] for b in batch]
+    xx = [torch.tensor(d['segment'], dtype=torch.float32) for d in batch]
+    xx_pad = pad_sequence(xx)
+    # print(xx_pad)
+    # yy_pad = pad_sequence(yy, batch_first=True, padding_value=0)
+    x_lens = [len(x) for x in xx]
+
+    return xx_pad, yy, x_lens
+
+
+dl = DataLoader(dataset=ds, batch_size=32, shuffle=True, collate_fn=pad_collate)
+xx_pad, yy, x_lens = next(iter(dl))
+print(xx_pad.shape)
+print(len(yy))
+print(x_lens)
+# %%
+
+# %%
+d = {'a': 1, 'b': 2}
+for k,v in d.items():
+    print(k, v)
+# %%
+d.values()
 # %%
