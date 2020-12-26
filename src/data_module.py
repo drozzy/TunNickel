a6538e4fd0140540bf0cde196f19454996a30010
Andriy Drozdyuk <andriy@drozdyuk.com>
2020-12-26 01:38:09 -0500

    Got rid of testing validation split since early stopping didnt work very well

--- src/data_module.py
+++ src/data_module.py
@@ -10,14 +10,12 @@ class JigsawsDataModule(pl.LightningDataModule):
         self.user_out = user_out
     
     def setup(self, stage=None):
-        ds = JigsawsDataset(task=self.task, user_out=self.user_out, train=True)
-        self.num_gestures = ds.num_gestures
-        n = len(ds)
-        nval = int(0.1*n)
-        ntrain = n - nval
-        self.train_dataset, self.val_dataset = torch.utils.data.random_split(ds, [ntrain, nval], generator=torch.Generator().manual_seed(42))
+        self.train_dataset = JigsawsDataset(task=self.task, 
+                user_out=self.user_out, train=True)
+        self.val_dataset = JigsawsDataset(task=self.task, 
+                user_out=self.user_out, train=False)
 
-        self.test_dataset = JigsawsDataset(task=self.task, user_out=self.user_out, train=False)
+        self.num_gestures = self.train_dataset.num_gestures
         
     def train_dataloader(self):
         return DataLoader(dataset=self.train_dataset, batch_size=32, shuffle=True, collate_fn=pad_collate)
@@ -25,8 +23,8 @@ class JigsawsDataModule(pl.LightningDataModule):
     def val_dataloader(self):
         return DataLoader(dataset=self.val_dataset, batch_size=128, collate_fn=pad_collate)
 
-    def test_dataloader(self):
-        return DataLoader(dataset=self.test_dataset, batch_size=128, collate_fn=pad_collate)
+    #def test_dataloader(self):
+        #return DataLoader(dataset=self.test_dataset, batch_size=128, collate_fn=pad_collate)
 # %%
 # dm = JigsawsDataModule()
 # dm.prepare_data()
