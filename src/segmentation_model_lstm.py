1c85dae8a6ef8eaf0e80e22c8f82b03cca5ff6b7
Andriy Drozdyuk <andriy@drozdyuk.com>
2021-02-10 03:02:02 -0500

    Sequences of length 3853 dont work

---
+++ src/segmentation_model_lstm.py
@@ -0,0 +1,79 @@
+# %% Imports
+import torch.nn.functional as F
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch
+from torch.nn.utils.rnn import pack_padded_sequence as pack
+from torch.nn.utils.rnn import pad_packed_sequence as unpack
+
+class SegmentationLSTMModel(pl.LightningModule):
+    def __init__(self, lr=0.001, num_classes=15):
+        super().__init__()
+        self.num_classes=num_classes
+        hidden = 86
+        self.lr = lr
+        self.lstm = nn.LSTM(input_size=76, hidden_size=hidden, batch_first=True)
+        self.final = nn.Linear(hidden, num_classes)
+        self.accuracy_train = pl.metrics.Accuracy()
+        self.accuracy_test = pl.metrics.Accuracy()
+        self.accuracy_val = pl.metrics.Accuracy()
+
+    def forward(self, xx_pad, x_lens):
+        xx_packed = pack(xx_pad, x_lens, batch_first=True, enforce_sorted=False)
+        
+        yy_pred_packed, _ = self.lstm(xx_packed)
+        yy_pred, _yy_pred_lens = unpack(yy_pred_packed)
+        yy_pred = self.final(yy_pred)
+
+        return yy_pred.permute(1, 0, 2)
+
+
+    def training_step(self, batch, batch_idx):
+        # x, y, xlens = batch
+        xx_pad, yy_pad, x_lens, y_lens, xx_mask_pad = batch
+        yy_pred = self(xx_pad, x_lens)
+        print(f"yy_pred shape: {yy_pred.shape}")
+        yy_pred = yy_pred.reshape(-1, self.num_classes)
+        yy_target = yy_pad.view(-1)
+
+        print(f"yy pred: {yy_pred.shape}")
+        print(f"yy_target: {yy_target.shape}")
+        loss = F.cross_entropy(yy_pred, yy_target)
+        
+        self.log('train_loss', loss)
+        self.log("train_acc_step", self.accuracy_train(yy_pred, yy_target))
+        return loss
+
+    def training_epoch_end(self, outs):
+        self.log("train_acc_epoch", self.accuracy_train.compute())
+
+    # def validation_step(self, batch, batch_idx):
+    #     x, y, xlens = batch
+    #     y_pred_logits = self(x)
+    #     loss = F.cross_entropy(y_pred_logits, y)
+        
+    #     self.log('val_loss', loss)
+    #     self.log("val_acc_step", self.accuracy_val(y_pred_logits, y))
+    #     return loss
+    
+    # def validation_epoch_end(self, outs):
+    #     self.log("val_acc_epoch", self.accuracy_val.compute(), prog_bar=True)
+
+    # def test_step(self, batch, batch_idx):
+    #     x, y, xlens = batch
+    #     y_pred_logits = self(x)
+    #     loss = F.cross_entropy(y_pred_logits, y)
+        
+    #     self.log('test_loss', loss)
+    #     self.log("test_acc_step", self.accuracy_test(y_pred_logits, y))
+    #     return loss
+    
+    # def test_epoch_end(self, outs):
+    #     self.log("test_acc_epoch", self.accuracy_test.compute())
+
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+# %%
---
+++ src/segmentation_test.py
@@ -0,0 +1,39 @@
+# %% Imports
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from segmentation_dataset_louo import SegmentationDataset
+from segmentation_model_lstm import SegmentationLSTMModel
+from segmentation_datamodule_louo import SegmentationDataModuleLouo
+# from pytorch_lightning.loggers import WandbLogger
+import datetime
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+
+data = SegmentationDataModuleLouo()
+data.prepare_data()
+data.setup()
+model = SegmentationLSTMModel()
+
+EPOCHS = 10
+
+trainer = pl.Trainer(gpus=1, max_epochs=100)
+
+trainer.fit(model, data)
+
+# %% Test 
+#trainer.test(datamodule=data);
+
+# %%
+# %%
+#%% Test things out
+# xx,yy,xlens = next(iter(data.train_dataloader()))
+# print("LABELS ARE:")
+# print(yy[0:5])
+# print(xx.shape, yy.shape)
+# outs = model(xx)
+# print(outs.shape)
+# print(outs[0])
+#import torch
+#torch.utils.data.random_split(range(10), [0.1, 0.9], generator=torch.Generator().manual_seed(42))
+# %%
