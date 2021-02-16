35fe14c118fee8e776d8d9034a1022278d461c55
Andriy Drozdyuk <andriy@drozdyuk.com>
2021-02-16 15:11:47 -0500

    add

--- src/scratch.py
+++ src/scratch.py
@@ -1,22 +1,8 @@
-# %%
-from tunnickel.model import Model, Module
+# %% 
 from importlib import resources
-import torch
-from pytorch_lightning import Trainer
-import torch.nn.functional as F
-from tunnickel.data import TrialsDataModule, USERS, NUM_LABELS
-from importlib import resources
-import torch
-# %%
 
-trainer = Trainer(max_epochs=2)
+from tunnickel.train import train_with_users
+from tunnickel.data import TrialsDataModule, USERS
 with resources.path("tunnickel", f"Suturing") as trials_dir:
-    mo = Module(num_features=76, num_classes=NUM_LABELS)
-    
-    batch_size = 3
-    dm = TrialsDataModule(trials_dir, test_users=[USERS[0]], train_batch_size=batch_size)
-   
-    result = trainer.fit(mo, datamodule=dm)
-
-trainer.test()
+    train_with_users(test_users=[USERS[0]], max_epochs=1, trials_dir=trials_dir)
 # %%
---
+++ src/tunnickel/train.py
@@ -0,0 +1,40 @@
+# %%
+from tunnickel.model import Model, Module
+from tunnickel.data import TrialsDataModule, USERS, NUM_LABELS
+import torch
+from pytorch_lightning import Trainer
+import torch.nn.functional as F
+from importlib import resources
+import torch
+
+def train_with_users(test_users, max_epochs, trials_dir, batch_size = 3):    
+    trainer = Trainer(max_epochs=max_epochs)
+    mo = Module(num_features=76, num_classes=NUM_LABELS)
+    
+    dm = TrialsDataModule(trials_dir, test_users=test_users, train_batch_size=batch_size)
+
+    result = trainer.fit(mo, datamodule=dm)
+
+    trainer.test()
+# %%
+# from tunnickel.model import Model, Module
+# from importlib import resources
+# import torch
+# from pytorch_lightning import Trainer
+# import torch.nn.functional as F
+# from tunnickel.data import TrialsDataModule, USERS, NUM_LABELS
+# from importlib import resources
+# import torch
+# # %%
+
+# trainer = Trainer(max_epochs=2)
+# with resources.path("tunnickel", f"Suturing") as trials_dir:
+#     mo = Module(num_features=76, num_classes=NUM_LABELS)
+    
+#     batch_size = 3
+#     dm = TrialsDataModule(trials_dir, test_users=[USERS[0]], train_batch_size=batch_size)
+   
+#     result = trainer.fit(mo, datamodule=dm)
+
+# trainer.test()
+# # %%
