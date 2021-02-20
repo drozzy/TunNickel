c21798f486bbe6dc342406044d3dd97ab23b53de
Andriy Drozdyuk <andriy@drozdyuk.com>
2021-02-20 09:28:29 -0500

    lstm res

--- src/tunnickel/model.py
+++ src/tunnickel/model.py
@@ -53,6 +53,27 @@ class LstmNeuralOdeModel(nn.Module):
          
         return x
 
+class LstmResNeuralOdeModel(nn.Module):
+    def __init__(self, num_features=76, num_classes=NUM_LABELS, hidden_size=32):
+        super().__init__()
+        self.func = LstmField(num_features=num_features)
+        self.neuralOde = NeuralODE(self.func)
+
+        # self.m = nn.Sequential(
+        #     self.neuralOde
+        # )
+        self.penultimate = nn.Linear(num_features, hidden_size)
+        self.final = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        x_orig = x
+        x = self.neuralOde(x)
+        x = x + x_orig
+        x = torch.relu(self.penultimate(x))
+        x = self.final(x)
+         
+        return x
+
 class PlainNeuralOdeModel(nn.Module):
     def __init__(self, num_features=76, num_classes=NUM_LABELS, hidden_size=32):
         super().__init__()
